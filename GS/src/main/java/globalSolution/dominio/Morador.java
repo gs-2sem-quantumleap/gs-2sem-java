99ea5279c892a5ea59c56f3ae8b67a22c2823020
Lucas-CMD <lucasalmeidafmoraes@gmail.com>
2024-11-12 20:52:49 -0300

    Adição Connection Factory

---
+++ GS/src/main/java/globalSolution/dominio/Morador.java
@@ -0,0 +1,55 @@
+package globalSolution.dominio;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Morador {
+
+    private long idMorador;
+    @JsonProperty
+    private String nomeMorador;
+    @JsonProperty
+    private String cpf;
+    @JsonProperty
+    private String email;
+    @JsonProperty
+    private String telefone;
+
+    public Morador(String nomeMorador, String cpf, String email, String telefone) {
+        this.nomeMorador = nomeMorador;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public String getNomeMorador() {
+        return nomeMorador;
+    }
+
+    public void setNomeMorador(String nomeMorador) {
+        this.nomeMorador = nomeMorador;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
